f5e4c6b5877a714b40ece0f439a9cb1db9b92702
shagunlamba <lamba.shagun0@gmail.com>
2021-02-04 15:59:47 +0530

    Add gitignore, profile and update port

--- app.js
+++ app.js
@@ -138,8 +138,12 @@ app.post("/delete",function(req,res){
     
 });
 
+let port = process.env.PORT;
+if(port== null || port ==""){
+    port= 3000;
+}
 
-app.listen(3000,function(req,res){
-    console.log("Server is running on port 3000");
+app.listen(port,function(){
+    console.log("Server is running");
 });
 
